c653059edf854efe3ab46d68cbe7ba037ad8f22b
vViniciuX <viniciusfranca960@gmail.com>
2024-01-09 15:07:46 -0300

    V1

--- Quiz/quiz.py
+++ Quiz/quiz.py
@@ -4,6 +4,7 @@ import math
 def new_quiz():
   corrects_answers = 0
   question_num = 0
+  
   for key in questions:
     print("----------- Quiz Game / Game -------------")
     print(key)
@@ -13,11 +14,13 @@ def new_quiz():
     corrects_answers += check_answer(questions.get(key), guess)
     
     question_num += 1
+    
   print("----------- Quiz Game / Results -------------")
   print("Score Rate:", math.floor((corrects_answers/len(questions))*100), "% (", corrects_answers, "/", len(questions), ")")
   play_again = input("Would you like play again? (yes or no): ")
   if play_again.lower() == "yes" or play_again.lower() == 'y':
     new_quiz()
+  
   
 def check_answer(answer, guess):
   if answer == guess.upper():
@@ -35,6 +38,7 @@ def edit_mode():
     print("Teste")
   elif question == '-':
     print("Teste")
+
 
 with open("questions.json") as questions:
   file_json = json.load(questions)
