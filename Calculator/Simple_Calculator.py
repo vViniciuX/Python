b0518d1e573bfc1888cf422acc3c952a10f056b8
vViniciuX <viniciusfranca960@gmail.com>
2024-01-10 20:32:03 -0300

    coding calculator

---
+++ Calculator/Simple_Calculator.py
@@ -0,0 +1,15 @@
+number1 = input("Number 1: ")
+operator = input("Operator: ")
+number2 = input("Number 2: ")
+
+match operator:
+  case '+':
+    print(int(number1)+int(number2))
+  case '-':
+    print(int(number1)+int(number2))
+  case '*':
+    print(int(number1)+int(number2))
+  case '/':
+    print(int(number1)+int(number2))
+    
+  
