e4465448cc6ccca44e98016a2ab258af26dc34e1
vViniciuX <viniciusfranca960@gmail.com>
2024-01-10 20:32:22 -0300

    coding calculate

--- Calculator/Simple_Calculator.py
+++ Calculator/Simple_Calculator.py
@@ -1,15 +1,19 @@
+def Calculate(number1, operator, number2):
+  print(number1 +operator +number2+'=')
+  match operator:
+    case '+':
+      print(int(number1)+int(number2))
+    case '-':
+      print(int(number1)+int(number2))
+    case '*':
+      print(int(number1)+int(number2))
+    case '/':
+      print(int(number1)+int(number2))
+
 number1 = input("Number 1: ")
 operator = input("Operator: ")
 number2 = input("Number 2: ")
 
-match operator:
-  case '+':
-    print(int(number1)+int(number2))
-  case '-':
-    print(int(number1)+int(number2))
-  case '*':
-    print(int(number1)+int(number2))
-  case '/':
-    print(int(number1)+int(number2))
+Calculate(number1, operator, number2)
     
   
